121723ca62d23883df262450e9e17c6abd0b501e
NauanVitor <142450673+NauanVitor@users.noreply.github.com>
2024-10-04 08:53:58 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 let estados = ['DF','GO','MT','MS','AL','BA','CE','MA','PB','PE','PI','RN','SE','AC','AP','AM','PA','RO','RR','TO','ES','MG','RJ','SP','PR','RS','SC'];
+let i= 0;
+while(i<10){
+console.log(i);
+i=i+1;
+}
