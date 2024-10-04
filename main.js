ca7931fa6564096c1d4a30b2c34027a982e5c099
NauanVitor <142450673+NauanVitor@users.noreply.github.com>
2024-10-04 09:11:31 -0300

    ,

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let estados = ['DF','GO','MT','MS','AL','BA','CE','MA','PB','PE','PI','RN','SE','AC','AP','AM','PA','RO','RR','TO','ES','MG','RJ','SP','PR','RS','SC'];
 let i= 0;
-while(i<estados length){
+while(i<estados.length){
 console.log(i,);
 console.log (estados[i]);
     i++;
