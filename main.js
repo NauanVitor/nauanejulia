f7fbaab34ae907420b0e07f685a4762b6ef011db
NauanVitor <142450673+NauanVitor@users.noreply.github.com>
2024-10-04 08:34:40 -0300

    ,

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-let estados =['DF','GO','MT','MS','AL','BA','CE','MA','PB','PE','PI','RN','SE','AC','AP','AM','PA','RO','RR','TO','ES','MG','RJ','SP','PR','RS','SC']
+let estados =['DF','GO','MT','MS','AL','BA','CE','MA','PB','PE','PI','RN','SE','AC','AP','AM','PA','RO','RR','TO','ES','MG','RJ','SP','PR','RS','SC'];
